2f4ed3020b1f0e5c6ad8925b33ce775b86f5eb16
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2025-03-27 10:19:44 +0000

    updated file

--- locustfile.py
+++ locustfile.py
@@ -6,9 +6,9 @@ class APIUser(HttpUser):
     wait_time = between(1, 3)  # Users wait between 1-3 seconds before making a new request
     file_uploaded = False  # Track if file upload was successful
 
-    @task(3)  # This task will run 3x more often than others
+    @task(3) 
     def predict(self):
-        """Send a prediction request with random test data."""
+        # Send a prediction request with random test data
         data = {
             "Age": random.randint(18, 45),
             "SystolicBP": random.randint(90, 180),
@@ -17,18 +17,18 @@ class APIUser(HttpUser):
             "BodyTemp": round(random.uniform(36.0, 39.0), 1),
             "HeartRate": random.randint(60, 130)
         }
-        response = self.client.post("/predict/", params=data)  # Using query parameters
+        response = self.client.post("/predict/", params = data)
         print("Predict Response:", response.status_code, response.text)
 
     @task(1)
     def upload_data(self):
-        """Upload the dataset before retraining."""
+        # Upload the dataset before retraining
         file_path = "Maternal Health Risk Data Set.csv"
         
         if os.path.exists(file_path):
             with open(file_path, "rb") as file:
                 files = {"file": file}
-                response = self.client.post("/upload/", files=files)
+                response = self.client.post("/upload/", files = files)
                 
                 if response.status_code == 200:
                     response_json = response.json()
@@ -44,7 +44,7 @@ class APIUser(HttpUser):
 
     @task(1)
     def retrain(self):
-        """Retrain model only if the file was uploaded successfully."""
+        # Retrain model only if the file was uploaded successfully
         if APIUser.file_uploaded:
             response = self.client.post("/retrain/")
             print("Retrain Response:", response.status_code, response.text)
